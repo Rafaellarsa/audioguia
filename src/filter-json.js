7b579dd2c2e3ddba92400ad362110045336167bc
Rafaellarsa <46501003+Rafaellarsa@users.noreply.github.com>
2020-11-09 11:48:09 -0300

    Teste com formato mp3

--- src/filter-json.js
+++ src/filter-json.js
@@ -8,6 +8,15 @@ const Information = [
         "numero": "02",
         "local": "../../assets/audios/Limon y sal/02-Me voy.flac"
     },
+
+    {
+        key: "03",
+        value: "Take me to church",
+        "nome": "Take me to church",
+        "numero": "03",
+        "local": "../../assets/audios/03. Hozier - Take Me To Church.mp3"
+    },
+
     {
         key: "04",
         value: "Limón y sal",
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Search from './pages/search/Search';
-import Player from './pages/home/App';
-import Player2 from './pages/home copy/App';
+import Player from './pages/audio/App';
+import Player2 from './pages/audio 2/App';
+import Player3 from './pages/audio 3/App';
 
 
 const Routes = () => (
@@ -11,7 +12,8 @@ const Routes = () => (
         <Switch>
             <Route path="/" exact component={Search} />
             <Route path="/audio/02" component={Player} />
-            <Route path="/audio/04" component={Player2} />
+            <Route path="/audio/03" component={Player2} />
+            <Route path="/audio/04" component={Player3} />
         </Switch>
     </BrowserRouter>
 );
