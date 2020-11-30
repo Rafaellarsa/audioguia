6e9a60293916d7e3f8db2a75dd58efa8987dfe36
Rafaellarsa <46501003+Rafaellarsa@users.noreply.github.com>
2020-11-29 22:20:18 -0300

    Style changes

--- src/pages/audio 3/App.js
+++ src/pages/audio 3/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import 'react-h5-audio-player/lib/styles.less';
 import logo from '../../assets/logo.png';
 import audioTeste from '../../assets/audios/Limon y sal/04-Limón y sal.flac';
 import Information from '../../filter-json';
@@ -13,10 +15,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1></h1>
-        <AudioPlayer
-    src={audioTeste}
-    onPlay={e => console.log("onPlay")}
-  />
+        <AudioPlayer src={audioTeste} onPlay={e => console.log("onPlay")}/>
         <Link
           className="App-link"
           to="/"
